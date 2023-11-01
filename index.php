147601f11babad5307f6fa3215f4151f9947a051
Adam Thomas <thomasadam83@hotmail.com>
2023-11-01 00:11:34 -0400

    Added core sample

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+include('includes/connect.php');
+include('includes/config.php');
+include('includes/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Home | Stop, Start, Continue</title>
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+</head>
+<body>
+
+    <div class="w3-container w3-section" style="max-width: 800px; margin: auto;">
+
+        <div class="w3-container w3-teal">
+
+            <h1>Header</h1>
+
+        </div>
+        
+    </div>
+
+</body>
+</html>
